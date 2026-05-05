4b4cec1cd01ea90c39394998961cf0f952b55523
Johannes <hainz.johannes00@gmail.com>
2026-05-05 22:13:05 +0200

    test-db- refactor

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://nbjnycgprwznxcikocxb.supabase.co',
+  supabaseKey: 'sb_publishable_LEnmq4BKDyiW8wJK3XX-3w_D01DBDCz'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://sjmfbdnzsktncbwqafjq.supabase.co',
+  supabaseKey: 'sb_publishable_jd7pQ3MgQBhs3vWTx0sweA__cq9BpI2'
+};
