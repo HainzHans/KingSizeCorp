ed34b67b8f39a9d59670bb8bddd788644c92048e
Johannes <hainz.johannes00@gmail.com>
2026-05-06 11:03:55 +0200

    sso

--- src/main.server.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
-
-const bootstrap = () => bootstrapApplication(App, appConfig);
-export default bootstrap;
